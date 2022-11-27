d6ce801afa938755f720a03a9e04cf2b03deb2e2
Chris Dee <christophe.deliquaire@gmail.com>
2022-11-27 15:06:51 +0100

    fix: code fixes

--- piboo/src/app/store.ts
+++ piboo/src/app/store.ts
@@ -1,7 +1,7 @@
 // For more information, see: https://redux-toolkit.js.org/usage/usage-with-typescript
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { AnyAction, configureStore, getDefaultMiddleware, ThunkDispatch } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-import appReducer from './appReducer';
+import appReducer, { AppState } from './appReducer';
 import cameraIpcMiddleware from './cameraIpcMiddleware';
 import captureMiddleware from './captureMiddleware';
 import printingIpcMiddleware from './printingIpcMiddleware';
@@ -20,7 +20,7 @@ export type AppDispatch = typeof store.dispatch
 
 // Export a hook that can be reused to resolve types
 // Use this instead of useDispatch
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppDispatch = () => useDispatch<AppDispatch & ThunkDispatch<AppState, null, AnyAction>>()
 
 export type AppStore = typeof store;
 
--- piboo/src/components/LivePreview.tsx
+++ piboo/src/components/LivePreview.tsx
@@ -1,27 +1,29 @@
 import { ipcRenderer } from 'electron';
-import React, { ReactNode } from 'react'; // we need this to make JSX compile
-import { connect } from 'react-redux';
-import { AppDispatch } from '../app/store';
+import React, { useEffect } from 'react'; // we need this to make JSX compile
+import { connect, useDispatch } from 'react-redux';
 import { bindToCamera } from '../app/reducers/captureControlSlice';
 
 type Message = any;
 type CameraManagerMessage = any;
 
-type LivePreviewProps = {
-  dispatch: AppDispatch,
+type ReactProps = {
   device: string,
 }
 
-class LivePreview extends React.Component<LivePreviewProps, {}, {}> {
+type LivePreviewProps = ReactProps;
 
-  myRef: React.RefObject<HTMLImageElement>;
-  onMessage = (event: Electron.IpcRendererEvent, arg0: any) => {
+const LivePreview = (props: LivePreviewProps) => {
+
+  const myRef: React.RefObject<HTMLImageElement> = React.createRef();
+  const dispatch = useDispatch();
+  
+  const onMessage = (event: Electron.IpcRendererEvent, arg0: any) => {
     const message = arg0 as Message;
     if(message.class === 0) { // MessageClass.CAMERA_MANAGER
       const cmm = message as CameraManagerMessage;
       switch (cmm.type) {
         case 5: // CAMERA_LIVEPREVIEW_FRAME
-          const img = this.myRef.current;
+          const img = myRef.current;
           if(img) {
             img.src = "data:image/jpeg;base64," + cmm.frame;
           }
@@ -32,29 +34,21 @@ class LivePreview extends React.Component<LivePreviewProps, {}, {}> {
     }
   }
 
-  constructor(props: LivePreviewProps) {
-    super(props);
-    const { dispatch } = props;
+  useEffect(() => {
     dispatch(bindToCamera(props.device));
-    this.myRef = React.createRef();
-  }
+    ipcRenderer.on('message', onMessage);
+    return () => {
+      ipcRenderer.removeListener('message', onMessage);
+    }
+  }, []);
 
-  componentDidMount() {
-    ipcRenderer.on('message', this.onMessage);
-  }
-
-  componentWillUnmount() {
-    ipcRenderer.removeListener('message', this.onMessage);
-  }
-
-  render(): ReactNode {
-    return <img 
-      ref={this.myRef}
+  return (
+    <img 
+      ref={myRef}
       width={960}
       height={540}
     />
-  }
-  
+  );
 }
 
 export default connect()(LivePreview);
