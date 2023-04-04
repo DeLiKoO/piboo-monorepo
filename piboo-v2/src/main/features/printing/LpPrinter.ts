06ef043682b36a63296e7f93f57f2aab223a03a6
Christophe Deliquaire <christophe.deliquaire@gmail.com>
2023-04-04 22:14:29 +0200

    fix: printer job cancellation on timeout (60s)

--- piboo-v2/src/main/features/printing/LpPrinter.ts
+++ piboo-v2/src/main/features/printing/LpPrinter.ts
@@ -7,7 +7,32 @@ enum JobState {
     COMPLETED,
 }
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));    
+const JOB_TIMEOUT_MS = 60 * 1000;
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const pexec = (command: string, args: string[]) => new Promise((resolve, reject) => {
+
+    const process = child_process.spawn(
+        command,
+        args,
+        {
+            timeout: 10000,
+        }
+    )
+    const chunks: string[] = [];
+    process.stdout.on("data", chunk => {
+        chunks.push(chunk.toString());
+    });
+    process.on("close", exitCode => {
+        if (exitCode === 0) {
+            resolve(chunks);
+        } else {
+            reject(new Error(`${command} exited with code ${exitCode}`));
+        }
+    });
+
+});
 
 // A printing method that uses the 'lp' command (linux)
 export default class LpPrinter implements Printer {
@@ -18,6 +43,10 @@ export default class LpPrinter implements Printer {
         this.settings = settings;
     }
 
+    async enable_printer_queue(): Promise<void> {
+        await pexec("cupsenable", [this.settings.printerName]);
+    }
+
     // returns promise of a job-id
     async start_print_job(filename: string): Promise<string> {
         return new Promise((resolve, reject) => {
@@ -25,7 +54,7 @@ export default class LpPrinter implements Printer {
             const { printerName, printerOptions } = settings;
             const lpArgs: string[] = [];
             lpArgs.push("-d", printerName);
-            for(const [key, value] of Object.entries(printerOptions)) {
+            for (const [key, value] of Object.entries(printerOptions)) {
                 lpArgs.push("-o");
                 lpArgs.push(`${key}=${value}`);
             }
@@ -38,18 +67,18 @@ export default class LpPrinter implements Printer {
                     timeout: 10000,
                 }
             )
-    
+
             const chunks: string[] = [];
             process.stdout.on("data", chunk => {
                 chunks.push(chunk.toString());
             });
             process.on("close", exitCode => {
-                if(exitCode === 0) {
+                if (exitCode === 0) {
                     const stdout = chunks.join("");
                     const lines = stdout.split(/\n/);
-                    for(let line of lines) {
+                    for (let line of lines) {
                         const match = line.match(/request id is ([^\s]+)/);
-                        if(match !== null && match.length > 1) {
+                        if (match !== null && match.length > 1) {
                             const jobId = match[1];
                             resolve(jobId);
                             return;
@@ -61,6 +90,10 @@ export default class LpPrinter implements Printer {
                 }
             });
         });
+    }
+
+    async cancel_print_job(jobId: string): Promise<void> {
+        await pexec("cancel", ["-a", jobId]);
     }
 
     async check_print_job(jobId: string): Promise<JobState> {
@@ -80,12 +113,12 @@ export default class LpPrinter implements Printer {
                 chunks.push(chunk.toString());
             });
             process.on("close", exitCode => {
-                if(exitCode === 0) {
+                if (exitCode === 0) {
                     const stdout = chunks.join("");
                     const lines = stdout.split(/\n/);
                     const entries = lines.map(entry => entry.replaceAll(/\s+/g, ";").split(';'));
                     const matching = entries.filter(e => e[0] === jobId);
-                    if(matching.length > 0) {
+                    if (matching.length > 0) {
                         resolve(JobState.NOT_COMPLETED);
                     } else {
                         resolve(JobState.COMPLETED);
@@ -100,11 +133,19 @@ export default class LpPrinter implements Printer {
     async print(filename: string): Promise<PrintResult> {
         const jobId = await this.start_print_job(filename);
         let jobState: JobState = JobState.NOT_COMPLETED;
+        const sentAt = Date.now();
         do {
             await sleep(250);
             jobState = await this.check_print_job(jobId);
+            const now = Date.now();
+            if (now - sentAt > JOB_TIMEOUT_MS) {
+                this.cancel_print_job(jobId);
+                await sleep(1000);
+                this.enable_printer_queue();
+                break;
+            }
         } while (jobState !== JobState.COMPLETED);
 
-        return { status: 'OK'};
+        return { status: jobState === JobState.COMPLETED ? 'OK' : 'ERROR' };
     }
 }
